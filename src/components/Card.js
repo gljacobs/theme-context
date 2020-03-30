d7eb54cdec25aa7f6ada5d89ad2d70303a56c21c
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-03-30 15:02:39 -0700

    add button styling that toggles on theme changre

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,12 +5,11 @@ function Card() {
 
     return (
         <ThemeContext.Consumer>{({ theme, toggleTheme, isLight }) => (
-
             <div class="card" style={{ width: "50vw", height: "200px", margin: "15vh auto", color: theme.secondary, background: theme.primary }}>
                 <div class="card-content center" style={{ padding: "40px" }}>
                     <span class="card-title">Change Theme</span>
                     <br />
-                    <button class="btn-large waves-effect waves-light" onClick={toggleTheme}>
+                    <button class="btn-large waves-effect waves-light" onClick={toggleTheme} style={isLight ? {} : {background: "#FFAB3F"}}>
                         {isLight  ? "Dark" : "Light"}
                         <i class="material-icons right">
                             {isLight ? "brightness_4" : "brightness_5"}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,6 @@ import { ThemeContext } from '../theme-context';
 function Nav() {
 
     return (
-
         <ThemeContext.Consumer>{({ theme }) => (
             <nav style={{ background: theme.primary }}>
                 <div className="nav-wrapper">
