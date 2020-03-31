9e072582a083191ef43e58b0cfdc98dd5b26db2e
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-03-31 12:37:51 -0700

    add button styling based on theme

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,13 +5,13 @@ function Card() {
 
     return (
         <ThemeContext.Consumer>{({ theme, toggleTheme, isLight }) => (
-            <div class="card" style={{ width: "50vw", height: "200px", margin: "15vh auto", color: theme.secondary, background: theme.primary }}>
-                <div class="card-content center" style={{ padding: "40px" }}>
-                    <span class="card-title">Change Theme</span>
+            <div className="card" style={{ width: "50vw", height: "200px", margin: "15vh auto", color: theme.secondary, background: theme.primary }}>
+                <div className="card-content center" style={{ padding: "40px" }}>
+                    <span className="card-title">Change Theme</span>
                     <br />
-                    <button class="btn-large waves-effect waves-light" onClick={toggleTheme} style={isLight ? {} : {background: "#FFAB3F"}}>
+                    <button className={"btn-large waves-effect waves-light " + (isLight ? "teal lighten-2" : "orange")} onClick={toggleTheme}>
                         {isLight  ? "Dark" : "Light"}
-                        <i class="material-icons right">
+                        <i className="material-icons right">
                             {isLight ? "brightness_4" : "brightness_5"}
                         </i>
                     </button>
